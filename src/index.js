b1e2dd6e17e8308a5f9c41c9b35d14064019e1a9
MarynaChernilevska <118733419+MarynaChernilevska@users.noreply.github.com>
2022-12-10 17:18:43 +0200

    remove html to js, add forecast

--- src/index.js
+++ src/index.js
@@ -19,6 +19,33 @@ if (minutes < 10) {
 }
 document.querySelector("#date").innerHTML = `${day}, ${hours}:${minutes}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+ <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/50d@2x.png"
+                alt=""
+                width="42"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max"> 18° </span>
+                <span class="weather-forecast-temperature-min"> 12° </span>
+              </div>
+            </div>
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input").value;
@@ -35,6 +62,7 @@ function searchCity(searchInput) {
 }
 
 searchCity("Kyiv");
+displayForecast();
 
 function showWeather(response) {
   let city = document.querySelector("#city");
